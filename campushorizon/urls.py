f4e5ae280113885169f6ebdc39d69f744ee64dd3
Neeraj Chormale <neerajchormale39@gmail.com>
2026-08-23 17:37:34 +0530

    deploy: add dedicated /health/ endpoint, run collectstatic during build, fast startCommand

--- campushorizon/urls.py
+++ campushorizon/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('health/', lambda r: HttpResponse('ok', content_type='text/plain')),
+    path('healthz/', lambda r: HttpResponse('ok', content_type='text/plain')),
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
 ]
